b6e89f871c21b52ee0d4cce6260edb4372295db7
Johnny Meneses <bilbo_vader@outlook.com>
2022-06-24 10:09:03 -0300

    Criando tela Login

--- kotlinandroid/app/src/main/java/com/johnny/kotlin_android/MainActivity.kt
+++ kotlinandroid/app/src/main/java/com/johnny/kotlin_android/MainActivity.kt
@@ -10,13 +10,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
         supportActionBar!!.hide()
 
 
