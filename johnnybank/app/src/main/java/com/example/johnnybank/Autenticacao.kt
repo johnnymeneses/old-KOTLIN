cf1adccbe359014395676e29e6adf48a92e290e0
Johnny Meneses <bilbo_vader@outlook.com>
2022-06-24 13:29:29 -0300

    Autenticacao simples

---
+++ johnnybank/app/src/main/java/com/example/johnnybank/Autenticacao.kt
@@ -0,0 +1,9 @@
+package com.example.johnnybank
+
+import android.content.Context
+import android.widget.Toast
+
+
+fun validaEntrada(nome: String, password: String): Boolean = (password=="admin")
+
+
--- johnnybank/app/src/main/java/com/example/johnnybank/Login.kt
+++ johnnybank/app/src/main/java/com/example/johnnybank/Login.kt
@@ -1,10 +1,13 @@
 package com.example.johnnybank
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.johnnybank.databinding.ActivityLoginBinding
 
-class Login : AppCompatActivity() {
+class Login : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -18,8 +21,36 @@ class Login : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.btnLogin.setOnClickListener(this)
 
 
     }
+
+    override fun onClick(view: View) {
+
+
+        if(view.id ==R.id.btn_login){
+
+            autenticacao()
+        }
+
+    }
+
+
+  fun autenticacao(){
+
+      val username = binding.editMail.text.toString()
+      val password = binding.editPassword.text.toString()
+
+      if(validaEntrada(username,password)){
+          Toast.makeText(this,"Bem Vindo", Toast.LENGTH_SHORT).show()
+      }else{
+          Toast.makeText(this,"Senha incorreta", Toast.LENGTH_SHORT).show()
+      }
+  }
+
+
+
+
+
 }
