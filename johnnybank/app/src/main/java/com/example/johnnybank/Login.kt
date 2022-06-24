38ad757eb00e4d4e1581c6c085fdc216381da8a4
Johnny Meneses <bilbo_vader@outlook.com>
2022-06-24 11:09:25 -0300

    View Binding

--- johnnybank/app/src/main/java/com/example/johnnybank/Login.kt
+++ johnnybank/app/src/main/java/com/example/johnnybank/Login.kt
@@ -2,14 +2,23 @@ package com.example.johnnybank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.johnnybank.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
+//        setContentView(R.layout.activity_login)
 
         supportActionBar!!.hide()
+
+        //Instanciando o View Binding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
 
 
     }
